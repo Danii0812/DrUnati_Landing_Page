6560820677e2a23e2539f9438526ccdfa0e3e996
Dani0812 <140689522+Danii0812@users.noreply.github.com>
2026-05-27 19:56:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ text:"Our parents doing, our current realities. As parents or aspiring parents o
 },
 
 {
-name:"@drsolowookere",
+name:"Dr. Sola Olowookere",
 role:"",
 text:"Well done DR M Healt Corner! EAP Nigeria is really looking forward to having you come share your knowledge with us again!!!"
 }
